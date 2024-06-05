fa077367cc63bb7e3100612032ee76404fbe90f5
Felipe Santos <felipecassiors@gmail.com>
2024-06-05 13:24:50 -0300

    Add tests

--- netdata/rootfs/usr/sbin/health.sh
+++ netdata/rootfs/usr/sbin/health.sh
@@ -8,4 +8,4 @@ get_config netdata_healthcheck_target
 export NETDATA_HEALTHCHECK_TARGET="${netdata_healthcheck_target}"
 
 # Call the original Netdata docker health check
-exec /usr/sbin/health.sh.orig
+exec /usr/sbin/health.sh.orig "${@}"
