1ba01aa69b56eb869f7a7dc638b750386c994296
Felipe Santos <felipecassiors@gmail.com>
2025-03-17 13:01:00 -0300

    Add support for S.M.A.R.T. out of the box

--- netdata/rootfs/opt/netdata-hass-addon/prepare_image.sh
+++ netdata/rootfs/opt/netdata-hass-addon/prepare_image.sh
@@ -2,33 +2,18 @@
 
 set -euxo pipefail
 
-apt-get update
-
-# the upstream image may add/remove gpg in the future, this is a safeguard
-if command -v gpg >/dev/null; then
-    remove_gpg=false
-else
-    apt-get install -y --no-install-recommends gnupg
-    remove_gpg=true
-fi
-
 # the upstream image may add/remove curl in the future, this is a safeguard
 if command -v curl >/dev/null; then
     remove_curl=false
 else
+    apt-get update
     apt-get install -y --no-install-recommends curl
     remove_curl=true
 fi
 
 mkdir -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg |
-    gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-chmod a+r /etc/apt/keyrings/docker.gpg
-
-if [[ "${remove_gpg}" == true ]]; then
-    apt-get purge -y --auto-remove gnupg
-fi
-unset remove_gpg
+curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+chmod a+r /etc/apt/keyrings/docker.asc
 
 if [[ "${remove_curl}" == true ]]; then
     apt-get purge -y --auto-remove curl
@@ -36,22 +21,24 @@ fi
 unset remove_curl
 
 arch="$(dpkg --print-architecture)"
+# shellcheck disable=SC2153
 version_codename="$(. /etc/os-release && echo "${VERSION_CODENAME}")"
-echo "deb [arch=${arch} signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian ${version_codename} stable" |
+echo "deb [arch=${arch} signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian ${version_codename} stable" |
     tee /etc/apt/sources.list.d/docker.list
 unset arch version_codename
 
 apt-get update
 
-# lm-sensors: useful enough for Home Assistant to have it as a default dependency
 # docker-ce-cli: needed by the run.sh script
-apt-get install -y --no-install-recommends lm-sensors docker-ce-cli
+# lm-sensors: useful enough for Home Assistant to have it as a default dependency
+# smartmontools: useful enough for Home Assistant to have it as a default dependency
+apt-get install -y --no-install-recommends docker-ce-cli lm-sensors smartmontools
+
+# clean up apt cache
+rm -rf /var/lib/apt/lists/*
 
 # relocate the original health check script so we can put ours in its place
 mv -f /usr/sbin/health.sh /opt/netdata-hass-addon/original_health.sh
 
 # the upstream image may move the run.sh script in the future, this is a safeguard
 stat /usr/sbin/run.sh
-
-# clean up apt cache
-rm -rf /var/lib/apt/lists/*
